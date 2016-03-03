d3dcb2489463dcaf19b208da0c99f1bb0e6f7b27
Matt Hughes <matthughes72@gmail.com>
2016-03-03 16:56:06 -0500

    3rd Commit

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
-# DS---Getting-and-Cleaning-Data---Ass01
 
-Matt01
+#Getting and Cleaning Data: Course Project
+##Introduction
 
+This repository contains my work for the course project for the Coursera course "Getting and Cleaning data", part of the Data Science specialization. What follows first are my notes on the original data.
 
+##About the Data
 
+###Training and Testing Data Sets
+These data sets include the raw results from both sets of experiments.
 
+###Other Data
+# Subject: The number label for each of the subjects in the experiment
+# Activity: The name of the activity for each result set
+
+##Script and TidyData.txt
+
+The script reads in data from all data sets included under the UIC directory
+Each Subject is paired with the approprieate activity and the results are combined and each column is appropriately renamed to match.  The means are calulated and 
+
+##About the Code Book
+
+The CodeBook.md file explains the transformations performed and the resulting data and variables.
--- run_analysis.R
+++ run_analysis.R
@@ -1,37 +1,60 @@
 
-#This function will recurse through a web of UCI directories and ingest the data files into a single dataframe.
-#There are 2 parts that aren't independently variable:
-# - the parent directories have to be hard coded ('test' or 'train')
-# - the column names have to be hardcoded.
-#The function returns a data frame of column types Factor and int.
-
 run_analysis <- function() {
-  #directories <- c("UCI/test/", "UCI/train/")
-  #hard code the parent directory:
-  directories <- c("UCI/test/")
-  for (d in directories) {
-    #create a List of file names in the child directories.
-    file_list <- list.files(d, recursive=TRUE)
-    for (file in file_list) {
-      #check if the data frame already exists in the environment.
-      if (!exists("dataset")) {
-        #paste together the directory/file name (same as in else block)
-        dataset <-
-          read.csv(paste0(d, file))
-        colnames(dataset) <- file
-      } else {
-       tempData <-
-         read.csv(paste0(d, file))
-        colnames(tempData) <- file
-        dataset <- cbind(dataset, tempData)
-        rm(tempData)
-      }
-    }
-  }
 
-#Hard coded column names.  Would like to fix this:  
-colnames(dataset) <- (c("test.bodyaccX", "test.bodyaccY", "test.bodyaccZ", "test.bodygyroX", "test.bodygyroY", "test.bodygyroZ", "test.totalaccX", "test.totalaccY", "test.totalaccZ", "test.subject", "test.X", "test.Y"))
+  library(plyr)
+  
 
-  return (dataset)
+  # Load the datasets and merge
+  subjectTrain <- read.table("UCI/train/subject_train.txt")
+  xTrain <- read.table("UCI/train/X_train.txt")
+  yTrain <- read.table("UCI/train/y_train.txt")
+  
+  subjectTest <- read.table("UCI/test/subject_test.txt", header=FALSE)
+  xTest <- read.table("UCI/test/X_test.txt")
+  yTest <- read.table("UCI/test/y_test.txt")
+  
+  # Createing cols Sub, Features and Activity with meanfull names
+  colSub <- rbind(subjectTrain, subjectTest)
+  names(colSub) <- c("Subject")
+  
+  colActivity <- rbind(yTrain, yTest)
+  names(colActivity)<- c("Activity")
+  
+  colFeatures <- rbind(xTrain, xTest)
+  # reading features for the names
+  featuresNames <- read.table("UCI/features.txt")
+  names(colFeatures) <- featuresNames$V2
+  
+  # Creating the dataset
+  data <- cbind(colSub, colActivity, colFeatures)
+  
+  ### 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+  
+  # grep rows with mean and std
+  valNames <- grep("mean|std", featuresNames$V2, value=TRUE)
+  
+  # list for the names to select
+  names <- c(as.character(valNames), "Activity", "Subject")
+  data <- subset(data, select=names)
+  
+  ### 3. Uses descriptive activity names to name the activities in the data set
+  activityNames <- read.table("UCI/activity_labels.txt")
+  data$Activity <- activityNames[data$Activity, 2]
+  
+  ### 4. Appropriately labels the data set with descriptive variable names.
+  names(data)<-gsub("^t", "time", names(data))
+  names(data)<-gsub("^f", "frequency", names(data))
+  names(data)<-gsub("Acc", "Accelerometer", names(data))
+  names(data)<-gsub("Gyro", "Gyroscope", names(data))
+  names(data)<-gsub("Mag", "Magnitude", names(data))
+  names(data)<-gsub("BodyBody", "Body", names(data))
+  
+  # 5. From the data set in step 4, creates a second, independent tidy data set 
+  #with the average of each variable for each activity and each subject.
+  
+  data2<-aggregate(. ~Subject + Activity, data, mean)
+  data2<-data2[order(data2$Subject,data2$Activity),]
+  write.table(data2, file = "tidydata.txt",row.name=FALSE)
 
+  
 }
